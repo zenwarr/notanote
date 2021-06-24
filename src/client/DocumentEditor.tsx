3bf474d97f2b8eadeb2217d6e8f8aa8a25b4dd19
zenwarr <zenw@yandex.ru>
2021-06-24 14:35:02 +0600

    feat: title editor

--- src/client/DocumentEditor.tsx
+++ src/client/DocumentEditor.tsx
@@ -1,19 +1,25 @@
-import { useEffect, useRef } from "react";
+import * as React from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import { Document } from "./Document";
 import { DocumentManager } from "./DocumentManager";
 import "./DocumentEditor.css";
 import { EditorView } from "@codemirror/view";
+import { makeStyles } from "@material-ui/core";
 
 
 export type DocumentEditorProps = {
   fileId: string;
   doc: Document;
+  onTitleChange?: (title: string) => void;
 }
 
 
 export function DocumentEditor(props: DocumentEditorProps) {
   const containerRef = useRef<any>();
+  const titleInputRef = createRef<HTMLInputElement>();
   const viewRef = useRef<EditorView>();
+  const classes = useStyles();
+  const [ title, setTitle ] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     const view = new EditorView({
@@ -30,8 +36,44 @@ export function DocumentEditor(props: DocumentEditorProps) {
   }, []);
 
   useEffect(() => {
-    viewRef.current?.focus();
+    titleInputRef.current?.focus();
   }, [ props.doc ]);
 
-  return <div ref={ containerRef }/>;
+  function onTitleKey(e: React.KeyboardEvent) {
+    if (e.key === "Enter") {
+      viewRef.current?.focus();
+      e.preventDefault();
+    }
+  }
+
+  function onTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let title = e.target.value;
+    setTitle(title);
+    props.onTitleChange?.(title);
+  }
+
+  return <div>
+    <input type={ "text" } ref={ titleInputRef } className={ classes.titleInput } onKeyDown={ onTitleKey } value={ title }
+           onChange={ onTitleChange }/>
+
+    <div ref={ containerRef }/>
+  </div>;
 }
+
+
+const useStyles = makeStyles(theme => ({
+  titleInput: {
+    width: "100%",
+    marginBottom: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    border: 0,
+    background: "transparent",
+    fontSize: "25px",
+    fontWeight: "bold",
+
+    "&:focus": {
+      outline: "none"
+    }
+  }
+}));
