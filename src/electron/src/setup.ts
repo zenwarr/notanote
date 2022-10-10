dbb4e04c6819688e37f897a7115885d7d96b99de
zenwarr <zenw@yandex.ru>
2022-10-10 12:23:08 +0600

    fix: appimage icon

--- src/electron/src/setup.ts
+++ src/electron/src/setup.ts
@@ -99,7 +99,7 @@ export class ElectronCapacitorApp {
   }
 
   async init(): Promise<void> {
-    const icon = nativeImage.createFromPath(join(app.getAppPath(), 'assets', 'icon.png'));
+    const icon = nativeImage.createFromPath(join(app.getAppPath(), 'assets', 'icon512x512.png'));
     this.mainWindowState = windowStateKeeper({
       defaultWidth: 1000,
       defaultHeight: 800,
