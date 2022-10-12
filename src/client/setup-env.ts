4b1f7f2beb80405006b64d46551977dd22747012
zenwarr <zenw@yandex.ru>
2022-10-12 20:08:48 +0600

    fix: webapp init error

--- src/client/setup-env.ts
+++ src/client/setup-env.ts
@@ -1,7 +1,7 @@
 import { getPlatform, Platform } from "./platform/get-platform";
 
 
-if (getPlatform() === Platform.Android) {
+if (getPlatform() === Platform.Android || getPlatform() === Platform.Web) {
   (window as any).process = {
     env: {},
     cwd: () => "",
